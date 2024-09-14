115a718602fb1289816f49e6dea73b9d71152b47
itssayushhere <yayushman088@gmail.com>
2024-09-14 05:55:06 +0530

    updated any with linkprops

--- src/components/ui/navbar-menu.tsx
+++ src/components/ui/navbar-menu.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import { motion } from "framer-motion";
-import Link from "next/link";
+import Link, { LinkProps } from "next/link";
 import Image from "next/image";
 
 const transition = {
@@ -109,11 +109,11 @@ export const ProductItem = ({
   );
 };
 
-export const HoveredLink = ({ children, ...rest }: any) => {
+export const HoveredLink = ({ children, ...rest }: React.PropsWithChildren<LinkProps>) => {
   return (
     <Link
       {...rest}
-      className="text-neutral-700 dark:text-neutral-200 hover:text-neutral-400 "
+      className="text-neutral-700 dark:text-neutral-200 hover:text-neutral-400"
     >
       {children}
     </Link>
